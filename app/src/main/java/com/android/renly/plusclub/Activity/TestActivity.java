17654605ea7440bd9eb3f0d80836b23c45288d46
WithLei <syunki@yeah.net>
2018-09-01 16:33:44 +0800

    9.1

---
+++ app/src/main/java/com/android/renly/plusclub/Activity/TestActivity.java
@@ -0,0 +1,21 @@
+package com.android.renly.plusclub.Activity;
+
+import com.android.renly.plusclub.Common.BaseActivity;
+import com.android.renly.plusclub.R;
+
+public class TestActivity extends BaseActivity {
+    @Override
+    protected int getLayoutID() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
