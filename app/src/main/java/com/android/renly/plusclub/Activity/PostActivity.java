4791fe7c7c8b9ecc0baa745a7551c93e7c766927
WithLei <syunki@yeah.net>
2018-10-03 16:36:57 +0800

    10.3 框架测试完成

--- app/src/main/java/com/android/renly/plusclub/Activity/PostActivity.java
+++ app/src/main/java/com/android/renly/plusclub/Activity/PostActivity.java
@@ -1,11 +1,18 @@
 package com.android.renly.plusclub.Activity;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import com.android.renly.plusclub.Common.BaseActivity;
 import com.android.renly.plusclub.R;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,7 +39,44 @@ public class PostActivity extends BaseActivity {
     @Override
     protected void initView() {
         initToolBar(true,title);
+        initSlidr();
+        initSlidingLayout();
     }
+
+    private void initSlidingLayout() {
+        slidingLayout.setAnchorPoint(0.7f);
+        slidingLayout.setPanelState(PanelState.COLLAPSED);
+        slidingLayout.setFadeOnClickListener(view -> {
+            mAnimator = new ObjectAnimator().ofFloat(myToolBar,"translationY",myToolBar.getTranslationY(),0);
+            mAnimator.start();
+            isShow = true;
+            slidingLayout.setPanelState(PanelState.COLLAPSED);
+            printLog("触发点击");
+        });
+        slidingLayout.addPanelSlideListener(new SlidingUpPanelLayout.SimplePanelSlideListener(){
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                super.onPanelSlide(panel, slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, PanelState previousState, PanelState newState) {
+                if (newState == PanelState.DRAGGING){
+                    ppState = previousState;
+                    return;
+                }
+                if (newState == PanelState.HIDDEN
+                        || newState == PanelState.COLLAPSED
+                        ||(previousState == PanelState.DRAGGING && newState == PanelState.ANCHORED && ppState == PanelState.EXPANDED))
+                    animToolBar(false);
+                else
+                    animToolBar(true);
+            }
+        });
+    }
+
+    // 前前次的状态
+    PanelState ppState = PanelState.COLLAPSED;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,4 +89,38 @@ public class PostActivity extends BaseActivity {
         super.onDestroy();
         unbinder.unbind();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (slidingLayout != null &&
+                (slidingLayout.getPanelState() == PanelState.EXPANDED || slidingLayout.getPanelState() == PanelState.ANCHORED)) {
+            slidingLayout.setPanelState(PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    Animator mAnimator;
+    boolean isShow = true;
+    public void animToolBar(boolean isUP){
+        if (mAnimator != null && mAnimator.isRunning()){
+            printLog("return 状态");
+            return;
+        }
+
+        if (isUP && isShow){
+            // 向上滑动
+            isShow = false;
+            printLog("向上滑动");
+            mAnimator = new ObjectAnimator().ofFloat(myToolBar,"translationY",myToolBar.getTranslationY(),-myToolBar.getHeight());
+            mAnimator.start();
+        }else if (!isUP && !isShow){
+            // 向下滑动
+            isShow = true;
+            printLog("向下滑动");
+            mAnimator = new ObjectAnimator().ofFloat(myToolBar,"translationY",myToolBar.getTranslationY(),0);
+            mAnimator.start();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/android/renly/plusclub/Adapter/PostAdapter.java
@@ -0,0 +1,24 @@
+package com.android.renly.plusclub.Adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class PostAdapter extends RecyclerView.Adapter  {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
